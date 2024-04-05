2845fd40b9d06203f8ed568f9778ca91d37f28c4
LEE-HYUNJOON <113746851+LEE-HYUNJOON@users.noreply.github.com>
2024-04-05 19:56:48 +0900

    Update images.js

--- images.js
+++ images.js
@@ -1,56 +1,56 @@
 const imageArray = [
     {
-      image: "https://leep-hj.github.io/nomad-js/assets/01.jpg",
+      image: "/nomad-js/assets/01.jpg",
       wiseSaying:
         "Life is either a daring adventure or nothing at all. – Helen Keller",
     },
     {
-      image: "https://leep-hj.github.io/nomad-js/assets/02.jpg",
+      image: "/nomad-js/assets/02.jpg",
       wiseSaying:
         "The only way to do great work is to love what you do. – Steve Jobs",
     },
     {
-      image: "https://leep-hj.github.io/nomad-js/assets/03.jpg",
+      image: "/nomad-js/assets/03.jpg",
       wiseSaying:
         "The best way to predict the future is to create it. – Alan Kay",
     },
     {
-      image: "https://leep-hj.github.io/nomad-js/assets/04.jpg",
+      image: "/nomad-js/assets/04.jpg",
       wiseSaying:
         "In the end, it’s not the years in your life that count. It’s the life in your years. – Abraham Lincoln",
     },
     {
-      image: "https://leep-hj.github.io/nomad-js/assets/05.jpg",
+      image: "/nomad-js/assets/05.jpg",
       wiseSaying:
         "The greatest glory in living lies not in never falling, but in rising every time we fall. – Nelson Mandela",
     },
     {
-      image: "https://leep-hj.github.io/nomad-js/assets/06.jpg",
+      image: "/nomad-js/assets/06.jpg",
       wiseSaying:
         "You will face many defeats in life, but never let yourself be defeated. – Maya Angelou",
     },
     {
-      image: "https://leep-hj.github.io/nomad-js/assets/07.jpg",
+      image: "/nomad-js/assets/07.jpg",
       wiseSaying:
         "If you spend too much time thinking about a thing, you’ll never get it done. – Bruce Lee",
     },
     {
-      image: "https://leep-hj.github.io/nomad-js/assets/08.jpg",
+      image: "/nomad-js/assets/08.jpg",
       wiseSaying:
         "Keep your eyes on the stars and your feet on the ground. – Theodore Roosevelt",
     },
     {
-      image: "https://leep-hj.github.io/nomad-js/assets/09.jpg",
+      image: "/nomad-js/assets/09.jpg",
       wiseSaying:
         "Despite the forecast, live like it’s spring. –  Lilly Pulitzer",
     },
     {
-      image: "https://leep-hj.github.io/nomad-js/assets/10.jpg",
+      image: "/nomad-js/assets/10.jpg",
       wiseSaying:
         "The two most important days in your life are the day you are born and the day you find out why. –  Mark Twain",
     },
     {
-      image: "https://leep-hj.github.io/nomad-js/assets/11.jpg",
+      image: "/nomad-js/assets/11.jpg",
       wiseSaying:
         "Try not to become a man of success but rather try to become a man of value. -Albert Einstein",
     },
